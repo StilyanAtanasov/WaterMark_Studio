c61425f03c4148cb1d59108d7914ca1de5752552
Stilyan Atanasov <stilyan2008@gmail.com>
2023-12-09 14:44:18 +0200

    header optimization beta
    Now the header is genereted through one source - header.js and it is called from the HTML with parameters. Test version only for core.html page

--- my_app/core/static/header.js
+++ my_app/core/static/header.js
@@ -1,5 +1,64 @@
-// Open-Close animation
+"use strict";
 
+// Header HTML
+class Info extends HTMLElement {
+  connectedCallback() {
+    const defaultStyle = {
+      HomeLinkClass: "",
+      MyProfileLinkClass: "",
+      AboutLinkClass: "",
+      ContactUsLinkClass: "",
+    };
+
+    const customStyle = this.getAttribute("custom-style");
+    const mergedStyle = { ...defaultStyle, ...JSON.parse(customStyle) };
+
+    this.innerHTML = `
+    <div class="mobileLeftSide">
+      <div id="bars" class="bars">
+        <div id="bar1" class="bar"></div>
+        <div id="bar2" class="bar"></div>
+        <div id="bar3" class="bar"></div>
+      </div>
+      <a class="navlogo"> WaterMark Studio</a>
+    </div>
+
+    <div class="navWrap">
+      <a href="/" class="navlink ${mergedStyle.HomeLinkClass}">Home</a>
+      <a href="#" class="navlink underBuild ${mergedStyle.MyProfileLinkClass}">My Profile</a>
+      <a href="#" class="navlink underBuild ${mergedStyle.AboutLinkClass}">About</a>
+      <a href="#" class="navlink underBuild ${mergedStyle.ContactUsLinkClass}">Contact us</a>
+    </div>
+    <div class="sidebar">
+      <div class="dropdown">
+        <label id="dropdown__face" class="dropdown__face" htmlFor="dropdown">
+          <div class="buttonwrap">
+            <span class="dropdown__text navlogo">CREATE </span>
+            <i class="fa-solid fa-chevron-down"></i>
+          </div>
+        </label>
+
+        <ul id="dropdown__items" class="dropdown__items">
+          <li>
+            <a class="sidelink" href="/textWatermark">RSH Mark</a>
+          </li>
+          <li><a class="sidelink" href="/imageWatermark">MSB Mark</a></li>
+        </ul>
+      </div>
+      <div class="logoutContainer">
+        <a class="link" href="{{ url_for('registration_bp.logout') }}">
+          <span class="link--top">Logout</span>
+          <span class="link--bottom">Logout</span>
+        </a>
+      </div>
+    </div>`;
+  }
+}
+
+// Define the custom element
+customElements.define("header-element", Info);
+
+// Open-Close animation
 let bar1 = document.getElementById("bar1");
 let bar2 = document.getElementById("bar2");
 let bar3 = document.getElementById("bar3");
